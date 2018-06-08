2408e6786606238c1327ddac0ad52e5ef51e0037
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-08 02:47:36 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
 def alphabetize(arr)
   # code here
 end
+
+ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(arr)
+  arr.sort_by do |a| 
+    a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } 
+  end 
+end
